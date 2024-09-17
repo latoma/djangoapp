fce21d9769b2eb045f7aeeeb7aae3ac07a80d8a3
Toma Lahtinen <127959432+latoma@users.noreply.github.com>
2024-09-17 22:06:13 +0300

    Update production.py

--- azuresite/production.py
+++ azuresite/production.py
@@ -21,17 +21,14 @@ MIDDLEWARE = [
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'  
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-# DBHOST is only the server name, not the full URL
-hostname = os.environ['DBHOST']
-
-# Configure Postgres database; the full username is username@servername,
-# which we construct using the DBHOST value.
+conn_str = os.environ['AZURE_POSTGRESQL_CONNECTIONSTRING']
+conn_str_params = {pair.split('=')[0]: pair.split('=')[1] for pair in conn_str.split(' ')}
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ['DBNAME'],
-        'HOST': hostname + ".postgres.database.azure.com",
-        'USER': os.environ['DBUSER'],
-        'PASSWORD': os.environ['DBPASS'] 
+        'NAME': conn_str_params['dbname'],
+        'HOST': conn_str_params['host'],
+        'USER': conn_str_params['user'],
+        'PASSWORD': conn_str_params['password'],
     }
 }
